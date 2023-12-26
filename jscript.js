3bea72f8f9c9d2d80ad13b4211e2b4572ab7c507
elimarayag <marayagelisha@gmail.com>
2023-12-26 23:58:42 +0800

    modified

--- jscript.js
+++ jscript.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+    let currentIndex = 0;
+    const images = document.querySelectorAll('#image-slider img');
+
+    function showNextImage() {
+      images[currentIndex].style.display = 'none';
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].style.display = 'block';
+      setTimeout(showNextImage, 1000); 
+    }
+
+    showNextImage();
